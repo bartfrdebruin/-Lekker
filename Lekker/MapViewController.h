bc4040cb2edc1d56c739a25c0d6f8a24ac62f7ac
Bart de Bruin <bart.fr.debruin@gmail.com>
2015-11-02 14:07:51 +0100

    Created Lekker project and added a MapViewController with xib

---
+++ Lekker/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  Lekker
+//
+//  Created by Bart de Bruin on 02-11-15.
+//  Copyright © 2015 Bart & Alyson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
