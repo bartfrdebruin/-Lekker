2edc050a6b608dd3f94e13b92ec16e684160e3f5
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-03 16:45:46 +0100

    Deleted didReceiveMemoryWarning from MapViewController.m.

--- Lekker/MapViewController.m
+++ Lekker/MapViewController.m
@@ -28,21 +28,6 @@
     [self.mapView setShowsUserLocation:YES];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 #pragma mark Image Picker
 
 - (IBAction)takePicture:(id)sender {
