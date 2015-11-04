9de26cb2cc5a0a96cdf873de589b6d2aa0a9192f
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-04 15:27:39 +0100

    Made AddLekkerViewController a delegate of UITextField.

--- Lekker/AddLekkerViewController.h
+++ Lekker/AddLekkerViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "MapViewController.h"
 
-@interface AddLekkerViewController : UIViewController <UINavigationControllerDelegate>
+@interface AddLekkerViewController : UIViewController <UINavigationControllerDelegate, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
