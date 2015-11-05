722ece5259267f46c5396df55973db5ec403e924
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-05 09:53:23 +0100

    Recreated & rewrote AddLekkerViewController because I totally messed up yesterday trying to do some Parse stuff.

---
+++ AddLekkerViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddLekkerViewController.h
+//  Lekker
+//
+//  Created by Alyson Vivattanapa on 11/5/15.
+//  Copyright © 2015 Bart & Alyson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MapViewController.h"
+
+@interface AddLekkerViewController : UIViewController <UINavigationControllerDelegate>
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) UIImage *photo;
+
+@end
---
+++ AddLekkerViewController.m
@@ -0,0 +1,25 @@
+//
+//  AddLekkerViewController.m
+//  Lekker
+//
+//  Created by Alyson Vivattanapa on 11/5/15.
+//  Copyright © 2015 Bart & Alyson. All rights reserved.
+//
+
+#import "AddLekkerViewController.h"
+#import "MapViewController.h"
+
+@interface AddLekkerViewController ()
+
+@end
+
+@implementation AddLekkerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    self.imageView.image = self.photo;
+    
+}
+
+@end
