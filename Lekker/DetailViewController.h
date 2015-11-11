18104c5aa80f6346598d6827992082a1778bab17
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-11 10:18:53 +0100

    Created DetailViewController file.

---
+++ Lekker/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Lekker
+//
+//  Created by Alyson Vivattanapa on 11/11/15.
+//  Copyright © 2015 Bart & Alyson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
