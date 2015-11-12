fff3305ada7d1910c96ca231496a273d7aa332e3
Alyson Vivattanapa <alyson.vivattanapa@gmail.com>
2015-11-12 14:43:22 +0100

    Set DetailViewController as a delegate of UINavigationController.

--- Lekker/DetailViewController.h
+++ Lekker/DetailViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "Lekker.h"
 
-@interface DetailViewController : UIViewController
+@interface DetailViewController : UIViewController <UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *lekkerImage;
 
